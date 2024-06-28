d060542e0b676df4bf0a2661dbdea0f547c29237
Павел Борисенко <pavel@air-pavel.speedport.ip>
2024-06-28 18:38:10 +0200

    fix

--- console.php
+++ console.php
@@ -8,4 +8,9 @@ $bus->setBaggage_weight(300);
 $bus->setVitrati_paliva_100_km(20);
 $bus->setMaximum_travel_distance(200);
 $bus->setFactor(2);
+
+$bus->setDistance(100);
+$bus->setKilometer_rate(5);
+$bus->setWater_category_coefficient(2);
+
 echo $bus->calculate();
